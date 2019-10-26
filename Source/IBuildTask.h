8f1e1a244248b5236b44e82b415dd25284d699b1
Matthew Asplund <mwasplund@outlook.com>
2019-10-26 11:36:11 -0700

    Add basic recipe and empty build task interface

---
+++ Source/IBuildTask.h
@@ -0,0 +1,17 @@
+// <copyright file="IBuildTask.h" company="Soup">
+// Copyright (c) Soup. All rights reserved.
+// </copyright>
+
+#pragma once
+
+namespace Soup::BuildEx
+{
+	/// <summary>
+	/// Build Task Extension interface
+	/// </summary>
+	class IBuildTask
+	{
+	public:
+		virtual const char* GetName() = 0;
+	};
+}
---
+++ Source/Module.cpp
@@ -0,0 +1,3 @@
+﻿export module Soup.BuildEx;
+
+#include "IBuildTask.h"
